51d8edd8ca794c99dd27499c0a6bb9731dcd6f03
Vivien Le Duc <leducvivien@gmail.com>
2023-04-10 16:16:19 +0200

    added github actions

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function foo() {
+    return "Hello World";
+}
+exports.default = foo;
+//# sourceMappingURL=index.js.map
