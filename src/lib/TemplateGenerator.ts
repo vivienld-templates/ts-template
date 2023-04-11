e2dbc7ffe979d874a0233bf7ae8a5d32b80b43e1
Vivien Le Duc <leducvivien@gmail.com>
2023-04-11 17:25:31 +0200

    first commit

--- src/lib/TemplateGenerator.ts
+++ src/lib/TemplateGenerator.ts
@@ -1,27 +1,53 @@
 import { mkdirp } from "mkdirp";
 import * as path from "path";
 
+/**
+ * Class to create a template directory.
+ */
 class TemplateGenerator {
+    /** The project name. */
     private _name: string;
+    /** The root directory path. */
+    private _rootDir: string;
+    /** The source directory path. */
+    private _srcDir: string;
+    /** The test directory path. */
+    private _testDir: string;
 
     constructor(name: string) {
         this.name = name;
+        this.rootDir = path.join(this.name);
+        this.srcDir = path.join(this.name, "src");
+        this.testDir = path.join(this.name, "test");
     }
 
     /**
-     * Generates the root directory
+     * Generates the root directory.
      */
-    generateRoot = (): void => {
-        this.generateDir(path.join(this.name));
+    generateRootDir = (): void => {
+        this.generateDir(this.rootDir);
     };
 
     /**
-     * Recursively generates the directories
+     * Generates the src directory.
+     */
+    generateSrcDir = (): void => {
+        this.generateDir(this.srcDir);
+    };
+
+    /**
+     * Generates the test directory.
+     */
+    generateTestDir = (): void => {
+        this.generateDir(this.testDir);
+    };
+
+    /**
+     * Recursively generates the directories.
      * @param path The directories path
      */
     private generateDir(path: string): void {
-        console.log(path);
-
+        console.log(`Generating ${path} dir(s)`);
         mkdirp.sync(path);
     }
 
@@ -32,6 +58,27 @@ class TemplateGenerator {
     }
     public set name(value: string) {
         this._name = value;
+    }
+
+    public get rootDir(): string {
+        return this._rootDir;
+    }
+    public set rootDir(value: string) {
+        this._rootDir = value;
+    }
+
+    public get srcDir(): string {
+        return this._srcDir;
+    }
+    public set srcDir(value: string) {
+        this._srcDir = value;
+    }
+
+    public get testDir(): string {
+        return this._testDir;
+    }
+    public set testDir(value: string) {
+        this._testDir = value;
     }
 }
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,30 @@
 import TemplateGenerator from "../src/lib/TemplateGenerator";
+import * as fs from "fs";
+import * as path from "path";
 
 const name = "project";
 const generator = new TemplateGenerator(name);
 
-it("should be able to generate a directory", () => {
-    generator.generateRoot();
-    expect(generator.name).toEqual(name);
+/**
+ * Tests root directory creation
+ */
+it("should be able to generate the root directory", () => {
+    generator.generateRootDir();
+    expect(fs.existsSync(generator.rootDir)).toBeTruthy();
+});
+
+/**
+ * Tests source directory creation
+ */
+it("should be able to generate the source directory", () => {
+    generator.generateSrcDir();
+    expect(fs.existsSync(generator.srcDir)).toBeTruthy();
+});
+
+/**
+ * Tests test directory creation
+ */
+it("should be able to generate the test directory", () => {
+    generator.generateTestDir();
+    expect(fs.existsSync(generator.testDir)).toBeTruthy();
 });
